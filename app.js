ea5bdc6e20779fb23aa785c9d8c4829553ae4d6e
rirchse <rirchse@gmail.com>
2024-08-27 23:43:49 +0600

    Database connection updated to push free hosting server

--- app.js
+++ app.js
@@ -21,11 +21,29 @@ const app = express();
 dotenv.config();
 
 //database connection
-main().catch(err => console.log(err));
-async function main() {
-  await mongoose.connect(process.env.MONGO_CONNECTION_STRING);
-  // console.log("Database Connection Successful.");
+// main().catch(err => console.log(err));
+// async function main() {
+//   await mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+//   // console.log("Database Connection Successful.");
+// }
+
+const uri = "mongodb+srv://mrirstt:z9mPjt7DbrClQ9KG@cluster0.e8jqd.mongodb.net/ejs-booking-app?retryWrites=true&w=majority&appName=Cluster0";
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await mongoose.disconnect();
+  }
 }
+run().catch(console.dir);
+//---------------- db connection ended --------
 
 // request parsers
 app.use(express.json());
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -10,7 +10,8 @@ const { checkLogin, requireRole } = require("../middlewares/common/checkLogin");
 const router = express.Router();
 
 // all methods from here
-router.get("/user", decorateHtml("User"), checkLogin, requireRole(["admin"]), userIndex);
+// router.get("/user", decorateHtml("User"), checkLogin, requireRole(["admin"]), userIndex);
+router.get("/user", decorateHtml("User"), userIndex);
 
 // remove user
 router.delete("/user/:id", removeUser);
